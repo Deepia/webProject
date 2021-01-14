7d3251c59d5e6bf7e42d7a08b9c2cf33fae9232b
Deepia <kumardeepak.parsa@gmail.com>
2021-01-14 16:43:17 +0530

    change port in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express= require("express");
 const path=require("path");
 const hbs=require("hbs");
 const app=express();
-const port=process.env.port || 8000;
+const port=process.env.PORT || 8000;
 const templates_path=path.join(__dirname,"../src/templates/views")
 const partial_path=path.join(__dirname,"../src/templates/partials")
 //for setting hbs
